26c558b1d67121b2891d81b351e77338e12a163b
Holden Gerlach <holdengerlach23@gmail.com>
2024-04-22 14:41:32 -0400

    product

--- src/App.jsx
+++ src/App.jsx
@@ -89,10 +89,98 @@ function App() {
           </div>
         </div>
         <div className="w-1/2 grid grid-rows-2 grid-cols-2 place-items-center">
-          <span><img src="https://cdn.doordash.com/media/consumer/home/landing/new/ScootScoot.svg" alt="" /></span>
-          <span><img src="https://cdn.doordash.com/media/consumer/home/landing/new/ScootScoot.svg" alt="" /></span>
-          <span><img src="https://cdn.doordash.com/media/consumer/home/landing/new/ScootScoot.svg" alt="" /></span>
-          <span><img src="https://cdn.doordash.com/media/consumer/home/landing/new/ScootScoot.svg" alt="" /></span>
+          <span><svg
+  width="100"
+  height="100"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 3V9H21V3H3ZM19 5H5V7H19V5Z"
+    fill="#fbbf24"
+  />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 11V21H11V11H3ZM9 13H5V19H9V13Z"
+    fill="#fbbf24"
+  />
+  <path d="M21 11H13V13H21V11Z" fill="#fbbf24" />
+  <path d="M13 15H21V17H13V15Z" fill="#fbbf24" />
+  <path d="M21 19H13V21H21V19Z" fill="#fbbf24" />
+</svg></span>
+          <span><svg
+  width="100"
+  height="100"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 3V9H21V3H3ZM19 5H5V7H19V5Z"
+    fill="#fbbf24"
+  />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 11V21H11V11H3ZM9 13H5V19H9V13Z"
+    fill="#fbbf24"
+  />
+  <path d="M21 11H13V13H21V11Z" fill="#fbbf24" />
+  <path d="M13 15H21V17H13V15Z" fill="#fbbf24" />
+  <path d="M21 19H13V21H21V19Z" fill="#fbbf24" />
+</svg></span>
+          <span><svg
+  width="100"
+  height="100"
+  viewBox="0 0 24 24"
+  fill="#fbbf24"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 3V9H21V3H3ZM19 5H5V7H19V5Z"
+    fill="#fbbf24"
+  />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 11V21H11V11H3ZM9 13H5V19H9V13Z"
+    fill="#fbbf24"
+  />
+  <path d="M21 11H13V13H21V11Z" fill="#fbbf24" />
+  <path d="M13 15H21V17H13V15Z" fill="#fbbf24" />
+  <path d="M21 19H13V21H21V19Z" fill="#fbbf24" />
+</svg></span>
+          <span><svg
+  width="100"
+  height="100"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 3V9H21V3H3ZM19 5H5V7H19V5Z"
+    fill="#fbbf24"
+  />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M3 11V21H11V11H3ZM9 13H5V19H9V13Z"
+    fill="#fbbf24"
+  />
+  <path d="M21 11H13V13H21V11Z" fill="#fbbf24" />
+  <path d="M13 15H21V17H13V15Z" fill="#fbbf24" />
+  <path d="M21 19H13V21H21V19Z" fill="#fbbf24" />
+</svg></span>
         </div>
       </section>
       <footer className="bg-zinc-800 text-gray-100 flex justify-center py-20 ">
